7d09a9a4e481f5afd169a332911f523a3d1f6768
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-02-17 17:20:28 +0200

    Create loops.cs

---
+++ loops.cs
@@ -0,0 +1,28 @@
+/*
+using System;
+
+namespace Firstproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter two numbers:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (x > y)
+            {
+                Console.WriteLine($"Number 1 > Number 2");
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("Number 1 < Number 2");
+            }
+            else
+            {
+                Console.WriteLine("Number 1 = Number 2");
+            }
+
+        }
+    }
+} */
