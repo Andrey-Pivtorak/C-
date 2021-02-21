164aed7d5fb25facd195d3913ef6efcf4e5ec7d4
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-02-21 15:13:16 +0200

    Update arrays.cs

--- arrays.cs
+++ arrays.cs
@@ -50,3 +50,49 @@ namespace Studyproject
 result
 {{{1 , 2} , {3 , 4}} , {{4 , 5} , {6 , 7}} , {{7 , 8}, {9 , 10}} , {{10 , 11} , {12 , 13}}}
 */
+
+/*
+using System;
+
+namespace SortApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // entering numbers
+            int[] nums = new int[7];
+            Console.WriteLine("Please, enter 7 numders:");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"Number {i+1}: ");
+                nums[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            // sorting numbers
+            int temp;
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+            // output
+            Console.WriteLine("Output the sorted array:");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+*/
+
