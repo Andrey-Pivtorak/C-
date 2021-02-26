98f4e7e161b65cc60bf1c344941e99ba4dcf0d1f
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-02-26 10:32:00 +0200

    Create methods.cs

---
+++ methods.cs
@@ -0,0 +1,31 @@
+/*
+using System;
+
+namespace StudyMethods
+{
+    class Program
+    {
+        static int Sum(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        static void PrintResult(int result)
+        {
+            Console.WriteLine("The result of Add: " + result);
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = Sum(a, b);
+
+            PrintResult(c);
+            Console.ReadKey();
+        }
+    }
+}
+*/
