195c839790846c1ddc4239dbc94468d26a15fe07
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-03-03 12:20:06 +0200

    Update arrays.cs

--- arrays.cs
+++ arrays.cs
@@ -238,3 +238,48 @@ namespace StudyProject
     }
 }
 */
+
+// The finding an index of the symbol
+/*
+using System;
+
+namespace MethodProject
+{
+    class Program
+    {
+        static int IndexOff(int [] array, int value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static int[] GetRandomArray(uint length, int minValue, int maxValue)
+        {
+
+            int[] myArray = new int[length];
+            Random random = new Random();
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = random.Next(minValue, maxValue);
+            }
+            return myArray;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] myArray = GetRandomArray(10, -20, 10);
+            int result = IndexOff(myArray, 23);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
+*/
