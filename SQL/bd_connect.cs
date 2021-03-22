495b036862141b740013c4132b775114e26f3924
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-03-22 15:18:22 +0200

    Update bd_connect.cs

--- SQL/bd_connect.cs
+++ SQL/bd_connect.cs
@@ -25,3 +25,22 @@ namespace BaseAppConnect
     }
 }
 */
+
+/*
+User.cs
+
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BaseAppConnect
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
+*/
