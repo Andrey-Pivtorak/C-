f7b4131fcd218e0b186035c61dd8d27c93526bb2
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-03-25 22:24:43 +0200

    Update sort_app.cs

--- SQL/sort_app.cs
+++ SQL/sort_app.cs
@@ -66,3 +66,22 @@ namespace SortApp.Controllers
     }
 }
 */
+
+/*
+Company.cs
+
+using System.Collections.Generic;
+
+namespace SortApp.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public List<User> Users { get; set; }
+        public Company()
+        {
+            Users = new List<User>();
+        }
+    }
+}
