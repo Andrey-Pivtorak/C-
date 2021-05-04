fe1058fee51d91412de25ba92df07bec6778f304
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-05-04 18:30:44 +0300

    Create CottonWash.cs

---
+++ OOP/WashingMashine/TypeOfProgramsWashing/CottonWash.cs
@@ -0,0 +1,7 @@
+namespace WashingMashine.TypeOfProgramsWashing
+{
+    public class CottonWash : PatternProgramsWashing
+    {
+        public CottonWash() : base("Хлопок", 0, 3, 800, "Выключен") { }
+    }
+}
