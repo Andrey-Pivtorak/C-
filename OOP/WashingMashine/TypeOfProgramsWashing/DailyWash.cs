0a2df2c226dba7af481b41fa2e037c43ae55cfcb
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-05-06 11:17:48 +0300

    Create DailyWash.cs

---
+++ OOP/WashingMashine/TypeOfProgramsWashing/DailyWash.cs
@@ -0,0 +1,8 @@
+
+namespace WashingMashine.TypeOfProgramsWashing
+{
+    public class DailyWash : PatternProgramsWashing
+    {        
+        public DailyWash() : base("Ежедневная стирка", 40, 2, 1200, "Включен") { }
+    }
+}
