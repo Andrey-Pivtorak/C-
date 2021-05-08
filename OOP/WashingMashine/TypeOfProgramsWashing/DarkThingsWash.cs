832fb150260505470f83221baa7e569b50059f8b
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-05-08 12:44:36 +0300

    Create DarkThingsWash.cs

---
+++ OOP/WashingMashine/TypeOfProgramsWashing/DarkThingsWash.cs
@@ -0,0 +1,8 @@
+
+namespace WashingMashine.TypeOfProgramsWashing
+{
+    public class DarkThingsWash : PatternProgramsWashing
+    {
+        public DarkThingsWash() : base("Темные вещи", 40, 3, 1200, "Включен") { }
+    }
+}
