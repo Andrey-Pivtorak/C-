31f1e77d6c92d2a9c25ba640ee40100f361fdfa1
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-05-09 22:14:30 +0300

    Create JeansWash.cs

---
+++ OOP/WashingMashine/TypeOfProgramsWashing/JeansWash.cs
@@ -0,0 +1,8 @@
+
+namespace WashingMashine.TypeOfProgramsWashing
+{
+    public class JeansWash : PatternProgramsWashing
+    {
+        public JeansWash() : base("Джинсы", 30, 3, 100, "Включен") { }
+    }
+}
