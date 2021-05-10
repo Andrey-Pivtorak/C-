c5b55fbdfbde5d27bbebabf4748621ca691a684d
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-05-10 09:28:59 +0300

    Create KidsThingsWas.cs

---
+++ OOP/WashingMashine/TypeOfProgramsWashing/KidsThingsWas.cs
@@ -0,0 +1,8 @@
+
+namespace WashingMashine.TypeOfProgramsWashing
+{
+    public class KidsThingsWash : PatternProgramsWashing
+    {
+        public KidsThingsWash() : base("Детские вещи", 95, 3, 1000, "Включен") { }
+    }
+}
