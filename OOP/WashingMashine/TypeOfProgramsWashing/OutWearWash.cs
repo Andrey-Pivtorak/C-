026d5d6c8bdcc38bcbe074b2be7b7a169c42c778
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-05-12 15:35:11 +0300

    Create OutWearWash.cs

---
+++ OOP/WashingMashine/TypeOfProgramsWashing/OutWearWash.cs
@@ -0,0 +1,8 @@
+
+namespace WashingMashine.TypeOfProgramsWashing
+{
+    public class OutWearWash : PatternProgramsWashing
+    {
+        public OutWearWash() : base("Верхняя одежда", 30, 3, 1200, "Выключен") { }
+    }
+}
