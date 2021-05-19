8a9833fbff83a02e0f4248256a28f322e14a13f5
Andrey Pivtorak <77983534+Andrey-Pivtorak@users.noreply.github.com>
2021-05-19 09:04:42 +0300

    Create SyntheticWash.cs

---
+++ OOP/WashingMashine/TypeOfProgramsWashing/SyntheticWash.cs
@@ -0,0 +1,8 @@
+
+namespace WashingMashine.TypeOfProgramsWashing
+{
+    public class SyntheticWash : PatternProgramsWashing
+    {
+        public SyntheticWash() : base("Синтетика", 30, 2, 1000, "Выключен") { }
+    }
+}
